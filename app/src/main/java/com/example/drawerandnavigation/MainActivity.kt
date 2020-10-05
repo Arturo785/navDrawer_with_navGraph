bd261c05d5161ca598535ba7e12a27c6ee8bbf0f
Jorge Arturo Rodriguez Martin <joarceus@hotmail.com>
2020-10-05 12:59:22 -0500

    Addition of nav_header, style, backArrow function and hide items fun

--- app/src/main/java/com/example/drawerandnavigation/MainActivity.kt
+++ app/src/main/java/com/example/drawerandnavigation/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
@@ -17,10 +18,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setSupportActionBar(toolBar)
+
         //This sets the drawer layout and the listener of the hamburger
         toogle = ActionBarDrawerToggle(this,navigation_drawer_layout,R.string.open,R.string.close)
         navigation_drawer_layout.addDrawerListener(toogle)
         toogle.syncState()
+
+        //hide some items
+        hideSomeItems()
 
         //To make the hamburger appear
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
@@ -35,5 +41,19 @@ class MainActivity : AppCompatActivity() {
             return true
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onBackPressed() {
+        if (navigation_drawer_layout.isDrawerOpen(GravityCompat.START)){
+            navigation_drawer_layout.closeDrawer(GravityCompat.START)
+        }
+        else{
+            super.onBackPressed()
+        }
+    }
+
+    private fun hideSomeItems(){
+        val menu = nav_view.menu
+        menu.findItem(R.id.nav_send).isVisible = false
     }
 }
